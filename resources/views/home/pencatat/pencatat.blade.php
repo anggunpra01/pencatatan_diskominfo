a7f1b2b3b2e39b9c949fcd18e841df92a3890284
Firda Rosiana T <firdaroziana10@gmail.com>
2022-09-07 09:42:17 +0700

    rename title

--- resources/views/home/pencatat/pencatat.blade.php
+++ resources/views/home/pencatat/pencatat.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Laporan</h1>
+    <h1 class="h2">Pencatat</h1>
   </div>
   <div class="table-responsive col-lg-15">
     <a href="/home/pencatat/create" class="btn btn-dark mb-3">Buat Laporan Baru</a>
